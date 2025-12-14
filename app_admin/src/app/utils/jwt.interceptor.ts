0a7a3b7ba13afd949218ed4cb0a4aa4a03b56cfc
Keith <keithpotz@gmail.com>
2025-12-14 12:31:08 -0500

    updated for module 7
    Update for module 7

---
+++ app_admin/src/app/utils/jwt.interceptor.ts
@@ -0,0 +1,44 @@
+import { Injectable, Provider } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { HttpInterceptor, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AuthenticationService } from "../services/authentication.service";
+
+
+@Injectable()
+export class jwtInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authService: AuthenticationService
+  ) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler):
+    Observable<HttpEvent<any>> {
+    var isAuthAPI: boolean;
+
+    if (request.url.startsWith('login') ||
+      request.url.startsWith('register')) {
+      isAuthAPI = true;
+    }
+    else {
+      isAuthAPI = false;
+    }
+
+    if (this.authService.isLoggedin() && !isAuthAPI) {
+      let token = this.authService.getToken();
+      const authReq = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(request);
+  }
+}
+
+export const authInterceptProvider: Provider =
+{
+  provide: HTTP_INTERCEPTORS,
+  useClass: jwtInterceptor, multi: true
+};
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
+//middleware for verifying JWT tokens
+function authenticateJWT(req, res, next){
+    const authHeader = req.headers['authorization'];
+
+    if(authHeader == null){
+       console.log('Auth header required but NOT PRESENT!');
+       return res.sendStatus(401);
+    }
+
+    let headers = authHeader.split(' ');
+    if(headers.length < 1){
+        console.log('Not enough tokens in auth header: ' + headers.length);
+        return res.sendStatus(501);
+    }
+    const token  = authHeader.split(' ')[1];
+    if(token == null){
+        console.log('Null bearer toekn');
+        return res.sendStatus(401);
+    }
+    const verfied = jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err){
+            return res.sendStatus(401).json('Token Validation Error!');
+        }
+        req.auth = verified;
+    });
+    next();
+}
+
+//import controllers for routing
 const tripsController = require('../controllers/trips');
+const authController = require('../controllers/authentication');
 
-router.route('/trips').get(tripsController.tripsList);
-router.route('/trips').post(tripsController.tripsAddTrip);
+router
+    .route('/register')
+    .post(authController.register);
 
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
-router.route('/trips/:tripCode').put(tripsController.tripsUpdateTrip);
+router
+    .route('/login')
+    .post(authController.login);
+
+//define routes  for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList)
+    .post(authenticateJWT,tripsController.tripsAddTrip);
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .put(authenticateJWT,tripsController.tripsUpdateTrip);
 
 module.exports = router;
